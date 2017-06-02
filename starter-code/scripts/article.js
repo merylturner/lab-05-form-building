e9f791425e0806956105b1e082d99b35ff452938
Meryl Turner <merylturner08@gmail.com>
2017-06-02 12:19:50 -0700

    finished TODOs in class, TODO review files

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -2,7 +2,7 @@
 
 var articles = [];
 
-function Article (rawDataObj) {
+function Article(rawDataObj) {
   this.author = rawDataObj.author;
   this.authorUrl = rawDataObj.authorUrl;
   this.title = rawDataObj.title;
@@ -11,10 +11,10 @@ function Article (rawDataObj) {
   this.publishedOn = rawDataObj.publishedOn;
 }
 
-Article.prototype.toHtml = function() {
+Article.prototype.toHtml = function () {
   var template = Handlebars.compile($('#article-template').text());
 
-  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000);
   this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
 
   // TODO (stretch goal): Pass the article body into the marked.js library to format our Markdown input!
@@ -22,14 +22,16 @@ Article.prototype.toHtml = function() {
   return template(this);
 };
 
-rawData.sort(function(a,b) {
-  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
-});
+function populateArticles() {
+  rawData.sort(function (a, b) {
+    return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+  });
 
-rawData.forEach(function(articleObject) {
-  articles.push(new Article(articleObject));
-})
+  rawData.forEach(function (articleObject) {
+    articles.push(new Article(articleObject));
+  })
 
-articles.forEach(function(article){
-  $('#articles').append(article.toHtml())
-});
+  articles.forEach(function (article) {
+    $('#articles').append(article.toHtml())
+  });
+}
--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -2,8 +2,8 @@
 
 var articleView = {};
 
-articleView.populateFilters = function() {
-  $('article').each(function() {
+articleView.populateFilters = function () {
+  $('article').each(function () {
     if (!$(this).hasClass('template')) {
       var val = $(this).find('address a').text();
       var optionTag = `<option value="${val}">${val}</option>`;
@@ -21,8 +21,8 @@ articleView.populateFilters = function() {
   });
 };
 
-articleView.handleAuthorFilter = function() {
-  $('#author-filter').on('change', function() {
+articleView.handleAuthorFilter = function () {
+  $('#author-filter').on('change', function () {
     if ($(this).val()) {
       $('article').hide();
       $(`article[data-author="${$(this).val()}"]`).fadeIn();
@@ -34,8 +34,8 @@ articleView.handleAuthorFilter = function() {
   });
 };
 
-articleView.handleCategoryFilter = function() {
-  $('#category-filter').on('change', function() {
+articleView.handleCategoryFilter = function () {
+  $('#category-filter').on('change', function () {
     if ($(this).val()) {
       $('article').hide();
       $(`article[data-category="${$(this).val()}"]`).fadeIn();
@@ -47,8 +47,8 @@ articleView.handleCategoryFilter = function() {
   });
 };
 
-articleView.handleMainNav = function() {
-  $('.main-nav').on('click', '.tab', function() {
+articleView.handleMainNav = function () {
+  $('.main-nav').on('click', '.tab', function () {
     $('.tab-content').hide();
     $('#' + $(this).data('content')).fadeIn();
   });
@@ -56,9 +56,9 @@ articleView.handleMainNav = function() {
   $('.main-nav .tab:first').click();
 };
 
-articleView.setTeasers = function() {
+articleView.setTeasers = function () {
   $('.article-body *:nth-of-type(n+2)').hide();
-  $('article').on('click', 'a.read-on', function(e) {
+  $('article').on('click', 'a.read-on', function (e) {
     e.preventDefault();
     if ($(this).text() === 'Read on →') {
       $(this).parent().find('*').fadeIn();
@@ -66,48 +66,64 @@ articleView.setTeasers = function() {
     } else {
       $('body').animate({
         scrollTop: ($(this).parent().offset().top)
-      },200);
+      }, 200);
       $(this).html('Read on &rarr;');
       $(this).parent().find('.article-body *:nth-of-type(n+2)').hide();
     }
   });
 };
 
-articleView.initNewArticlePage = function() {
+articleView.initNewArticlePage = function () {
   // TODO: Ensure the main .tab-content area is revealed. We might add more tabs later or otherwise edit the tab navigation.
-
+  $('.tab-content').show();
 
   // TODO: The new articles we create will be copy/pasted into our source data file.
   // Set up this "export" functionality. We can hide it for now, and show it once we have data to export.
-
-  $('#article-json').on('focus', function(){
+  $('#article-export').hide();
+  $('#article-json').on('focus', function () {
     this.select();
   });
 
   // TODO: Add an event handler to update the preview and the export field if any inputs change.
-
+  $('form').on('change', function () {
+    articleView.create();
+  });
 };
 
-articleView.create = function() {
+articleView.create = function () {
   // TODO: Set up a var to hold the new article we are creating.
   // Clear out the #articles element, so we can put in the updated preview
-
+  var $newArticle;
+  $('#articles').empty();
 
   // TODO: Instantiate an article based on what's in the form fields:
+  $newArticle = new Article({
+    title: $('#article-title').val(),
+    authorUrl: $('#author-url').val(),
+    author: $('#author-name').val(),
+    category: $('#category').val(),
+    body: $('#article-body').val(),
+    publishedOn: $('#published-on:checked').length > 0 ? new Date() : '(draft)'
+  });
 
+  console.log($newArticle);
 
   // TODO: Use our interface to the Handblebars template to put this new article into the DOM:
+  $('#articles').append($newArticle.toHtml());
 
 
   // TODO (stretch): Activate the highlighting of any code blocks; look at the documentation for hljs to see how to do this by placing a callback function in the .each():
   $('pre code').each();
 
   // TODO: Show our export field, and export the new article as JSON, so it's ready to copy/paste into blogArticles.js:
+  $('#article-export').show();
+  $('#article-json').val(JSON.stringify($newArticle));
 
 };
 
 
-articleView.initIndexPage = function() {
+articleView.initIndexPage = function () {
+  populateArticles();
   articleView.populateFilters();
   articleView.handleCategoryFilter();
   articleView.handleAuthorFilter();
